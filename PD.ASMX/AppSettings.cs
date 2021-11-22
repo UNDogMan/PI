329585a13a9731ac36df131339e1b692dcc10a51
Roman Zlobin <roman.zlobin@coral.team>
2021-11-22 09:51:08 +0300

    7b

---
+++ PD.ASMX/AppSettings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Configuration;
+using System.Globalization;
+
+public static class AppSettings
+{
+
+    public static string ClientSecret
+    {
+        get
+        {
+            return Setting<string>("ClientSecret");
+        }
+    }
+
+    public static bool UseDB
+    {
+        get
+        {
+            return Setting<bool>("UseDB");
+        }
+    }
+
+    private static T Setting<T>(string name)
+    {
+        string value = ConfigurationManager.AppSettings[name];
+
+        if (value == null)
+        {
+            throw new Exception(String.Format("Could not find setting '{0}',", name));
+        }
+
+        return (T)Convert.ChangeType(value, typeof(T), CultureInfo.InvariantCulture);
+    }
+}
